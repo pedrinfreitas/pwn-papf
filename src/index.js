728f0f8bbca2b7d322901d7cc79d7522b3925bc5
Pedro Freitas <pedrinfreitas@gmail.com>
2022-06-27 21:24:36 -0300

    fix: process.env.PORT

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
-
 const app = express();
+
+const port = process.env.PORT || 3333;
 
 app.get("/", (request, response) => {
   return response.send("olá mundo!");
 });
 
-app.listen(3333);
+app.listen(port, () => {
+  console.info("App rodando na porta 3333");
+});
